3b13c339cea23c3c4534c44c9ed342b3cf9172ea
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-04-28 18:52:33 -0700

    create function to generate task id(varibales, delete button), create function to check task's status

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -4,12 +4,52 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    return 'task-' + Date.now(); //need to check!!!!
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const taskCard = $('<div>')
+        .addClass('card task-card draggable my-3')
+        .attr('data-task-id', task.id);
+    const cardHeader = $('<div>')
+        .addClass('card-header h3')
+        .text(task.title);
+    const cardBody = $('<div>').addClass('card-body');
+    const cardDueDate = $('<div>')
+        .addClass('card-text')
+        .text(task.DueDate);
+    const cardDescription = $('<div>')
+        .addClass('card-text')
+        .text(task.description);
+    
+    //Create delete button
+    const cardDeleteButton = $('<button>')
+        .addClass('btn btn-danger delete')
+        .text('Delete')
+        .attr('data-task-id', task.id);
+    
+    cardDeleteButton.on('click', handleDeleteTask);
+   
+    //Create function to check the status of task and style the background color per provided criteria
+    if (task.DueDate && task.status !== 'done') {
+        const now = dayjs();
+        const taskDueDate = dayjs(task.dueDate, "DD/MM/YYYY");
 
+        if (now.isSame(taskDueDate, 'day')) {
+            taskCard.addClass('bg-warning text-white');
+        } else if (now.isAfter(taskDueDate)) {
+            taskCard.addClass('bg-danger text-white');
+            cardDeleteButton.addClass('border-light');
+        }
+    }
+
+    //Append all created elements to the cardBody and taskCard
+    cardBody.append(cardDueDate, cardDescription, cardDeleteButton);
+    taskCard.append(cardHeader, cardBody);
+
+    return taskCard;
+    
 }
 
 // Todo: create a function to render the task list and make cards draggable
